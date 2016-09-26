fd028cc15cc61d0838ca42853e7c9989ee5da745
PaRaLyZe <eric_20@hotmail.com>
2016-09-26 16:02:32 +0200

    mainJS
    added features

--- mx5bot.js
+++ mx5bot.js
@@ -7,6 +7,16 @@ bot.on("message", msg => {
         msg.channel.sendMessage("pong!");
     }
 });
+bot.on('guildMemberAdd', (g, m, msg) => {
+  console.log(`${m.user.username}`,"Joined",` ${g.name}`);
+
+})
+
+bot.on('guildMemberUpdate', (g, o, n, msg) => {
+  console.log("Username change old:",o.nickname,"to :", n.nickname);
+//  msg.channel.sendMessage("Welcome ", n.nickname," on the MX-5 Discord channel, enjoy your stay p.s. take a look at the rules");
+//  msg.channel.sendMessage("changed nick");
+});
 
 bot.on('ready', () => {
   console.log('I am ready!');
