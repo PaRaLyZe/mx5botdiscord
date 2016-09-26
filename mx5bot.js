2094aa116037f07ee6ca2ba8201f175d09c22aba
RWielinga <rwielinga@atoi.nl>
2016-09-26 16:10:35 +0200

    corrected whitespace

--- mx5bot.js
+++ mx5bot.js
@@ -2,28 +2,26 @@ var Discord = require("discord.js");
 var bot = new Discord.Client();
 
 //pingpong bot
-
 bot.on("message", msg => {
     if (msg.content.startsWith("!ping")) {
         msg.channel.sendMessage("pong!");
     }
 });
+
 // Join in console msg
 bot.on('guildMemberAdd', (g, m, msg) => {
   console.log(`${m.user.username}`,"Joined",` ${g.name}`);
-
 })
 
 //Nickname change
 bot.on('guildMemberUpdate', (g, o, n, msg) => {
   console.log("Username change old:",o.nickname,"to :", n.nickname);
-//  msg.channel.sendMessage("Welcome ", n.nickname," on the MX-5 Discord channel, enjoy your stay p.s. take a look at the rules");
-//  msg.channel.sendMessage("changed nick");
+  //  msg.channel.sendMessage("Welcome ", n.nickname," on the MX-5 Discord channel, enjoy your stay p.s. take a look at the rules");
+  //  msg.channel.sendMessage("changed nick");
 });
 
 bot.on('ready', () => {
   console.log('I am ready!');
-
 });
 
 bot.login("insert token here");
