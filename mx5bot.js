7ec81f92b2edff9c2503521a7b8cfc9df3283dcc
PaRaLyZe <eric_20@hotmail.com>
2016-09-26 16:05:33 +0200

    Test
    The master

--- mx5bot.js
+++ mx5bot.js
@@ -2,16 +2,19 @@ var Discord = require("discord.js");
 var bot = new Discord.Client();
 
 //pingpong bot
+
 bot.on("message", msg => {
     if (msg.content.startsWith("!ping")) {
         msg.channel.sendMessage("pong!");
     }
 });
+// Join in console msg
 bot.on('guildMemberAdd', (g, m, msg) => {
   console.log(`${m.user.username}`,"Joined",` ${g.name}`);
 
 })
 
+//Nickname change
 bot.on('guildMemberUpdate', (g, o, n, msg) => {
   console.log("Username change old:",o.nickname,"to :", n.nickname);
 //  msg.channel.sendMessage("Welcome ", n.nickname," on the MX-5 Discord channel, enjoy your stay p.s. take a look at the rules");
