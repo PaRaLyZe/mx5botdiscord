ccfc3cc1cd1d14a2425c44ea023720e40ed38c0f
PaRaLyZe <eric_20@hotmail.com>
2016-09-26 15:42:56 +0200

    MainJS
    Test

---
+++ mx5bot.js
@@ -0,0 +1,15 @@
+var Discord = require("discord.js");
+var bot = new Discord.Client();
+
+bot.on("message", msg => {
+    if (msg.content.startsWith("!ping")) {
+        msg.channel.sendMessage("pong!");
+    }
+});
+
+bot.on('ready', () => {
+  console.log('I am ready!');
+
+});
+
+bot.login("insert token here");
